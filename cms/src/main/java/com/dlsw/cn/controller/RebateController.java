92256787b86c1863480144715f3f11bb48605fe3
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-09-30 19:38:10 +0800

    init

--- cms/src/main/java/com/dlsw/cn/controller/RebateController.java
+++ cms/src/main/java/com/dlsw/cn/controller/RebateController.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/report")
 public class RebateController extends BaseController {
+
     @Autowired
     RebateService rebateService;
 
@@ -25,12 +25,12 @@ public class RebateController extends BaseController {
         List<Rebate> rebateList = rebateService.getRebateList(null);
         return new ResponseEntity<>(rebateList,HttpStatus.OK);
     }
+
     @ApiOperation(value = "更新返利状态")
     @RequestMapping(value = "/updateRebate", method = RequestMethod.POST)
     public  ResponseEntity<Map<String,Long>> updateRebate(){
         rebateService.updateRebate(null);
         return new ResponseEntity<>(HttpStatus.OK);
-
     }
 
 }
