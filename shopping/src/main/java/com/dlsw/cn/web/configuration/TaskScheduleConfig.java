2bdf6dbd70136b719db4bc3c0f528ba5522c52e4
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-10-09 16:07:47 +0800

    one to one 主键关联

--- shopping/src/main/java/com/dlsw/cn/web/configuration/TaskScheduleConfig.java
+++ shopping/src/main/java/com/dlsw/cn/web/configuration/TaskScheduleConfig.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,5 +28,10 @@ public class TaskScheduleConfig extends BaseService{
         log.debug("upgradeUerRoleType begin");
         restTemplate.getForEntity(url,null);
         log.debug("upgradeUerRoleType end");
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler(); //single threaded by default
     }
 } 
