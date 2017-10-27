c85c98ee5bc03950bfb8ed98e5ecacaa097dda7d
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-10-27 20:50:39 +0800

    old rebate and promote logic already have been delete

---
+++ shopping/src/main/java/com/dlsw/cn/web/service/OrderCheckService.java
@@ -0,0 +1,17 @@
+package com.dlsw.cn.web.service;
+
+import com.dlsw.cn.po.DeliveryAddress;
+import com.dlsw.cn.po.Order;
+import com.dlsw.cn.po.Product;
+import com.dlsw.cn.po.User;
+import com.dlsw.cn.web.vo.OrderVo;
+import com.dlsw.cn.web.vo.PayCertificateVo;
+
+public interface OrderCheckService {
+
+    void applyOrder(User user, Product product, User recommend_man, DeliveryAddress deliveryAddress, OrderVo orderVo);
+
+    void savePayCert(Order order, PayCertificateVo payCertificateVo);
+
+    void sureOrder(Order order, User user, User orderUser);
+}
