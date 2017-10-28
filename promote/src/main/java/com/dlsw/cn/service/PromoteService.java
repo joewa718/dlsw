a26e0cd873b57eecbd5cf7c2ba34b6665d4930a2
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-10-28 14:55:04 +0800

    等级升级

--- promote/src/main/java/com/dlsw/cn/service/PromoteService.java
+++ promote/src/main/java/com/dlsw/cn/service/PromoteService.java
@@ -1,5 +1,4 @@
 package com.dlsw.cn.service;
-
 import com.dlsw.cn.enumerate.RoleType;
 import com.dlsw.cn.po.User;
 import com.dlsw.cn.repositories.UserRepository;
@@ -25,7 +24,7 @@ public class PromoteService implements CommandLineRunner {
      * 监控VIP董事,满足3+6升级合伙人
      */
     public void monitorVip(){
-        logger.debug("-----------开始执行VIP升级任务-------------------");
+        logger.debug("-开始执行VIP升级任务-");
         List<User> vipList= userRepository.findByEqualsRoleType(RoleType.VIP);
         vipList.forEach(user -> {
             long oneLevel = userRepository.findSumByOneLevelOrgPath(user.getOrgPath(),RoleType.VIP);
@@ -34,17 +33,17 @@ public class PromoteService implements CommandLineRunner {
                 if(otherLevel >= 9){
                     user.setRoleType(RoleType.合伙人);
                     userRepository.save(user);
-                    logger.debug("-----------代理商:"+user.getNickname()+"("+user.getPhone()+") 升级到合伙人-------------------");
+                    logger.debug("-代理商:"+user.getNickname()+"("+user.getPhone()+") ,直属VIP("+oneLevel+"),团队VIP("+otherLevel+"),因此升级到合伙人-");
                 }
             }
         });
-        logger.debug("-----------结束执行VIP升级任务-------------------");
+        logger.debug("-结束执行VIP升级任务-");
     }
     /**
      * 监控合伙人董事,满足3+9升级高级合伙人
      */
     public void monitorPartner(){
-        logger.debug("-----------开始执行合伙人升级任务-------------------");
+        logger.debug("-开始执行合伙人升级任务-");
         List<User> vipList= userRepository.findByEqualsRoleType(RoleType.高级合伙人);
         vipList.forEach(user -> {
             long oneLevel = userRepository.findSumByOneLevelOrgPath(user.getOrgPath(),RoleType.高级合伙人);
@@ -53,11 +52,11 @@ public class PromoteService implements CommandLineRunner {
                 if(otherLevel >= 12){
                     user.setRoleType(RoleType.合伙人);
                     userRepository.save(user);
-                    logger.debug("-----------代理商:"+user.getNickname()+"("+user.getPhone()+") 升级到高级合伙人-------------------");
+                    logger.debug("-代理商:"+user.getNickname()+"("+user.getPhone()+") ,直属合伙人("+oneLevel+"),团队合伙人("+otherLevel+"),因此升级到高级合伙人-");
                 }
             }
         });
-        logger.debug("-----------结束执行合伙人升级任务-------------------");
+        logger.debug("-结束执行合伙人升级任务-");
     }
 
     @Override
