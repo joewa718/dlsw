1c762d437b207839fd7f6c370d89f9b6bd5cd835
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-10-28 14:50:48 +0800

    等级升级

--- promote/src/main/java/com/dlsw/cn/PromoteApplication.java
+++ promote/src/main/java/com/dlsw/cn/PromoteApplication.java
@@ -1,14 +1,22 @@
 package com.dlsw.cn;
 
+import com.dlsw.cn.service.PromoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.dlsw.cn.*"})
 public class PromoteApplication {
+	@Bean
+	public ExitCodeGenerator exitCodeGenerator() {
+		return () -> 42;
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(PromoteApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(PromoteApplication.class, args)));
 	}
 }
