4b46ebbe9e81e9201a072cde419f02542837ff5c
Wang Zhan <Wang.Zhan.ap@nielsen.com>
2017-11-05 12:17:08 +0800

    循环找上级代理商

---
+++ shopping/src/main/java/com/dlsw/cn/web/configuration/Config.java
@@ -0,0 +1,7 @@
+package com.dlsw.cn.web.configuration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.dlsw.cn.*")
+class Config {
+}
--- shopping/src/test/java/com/nielsen/cn/web/service/DirectorLevelTest.java
+++ shopping/src/test/java/com/nielsen/cn/web/service/DirectorLevelTest.java
@@ -5,46 +5,48 @@ import com.dlsw.cn.enumerate.DirectorLevel;
 import com.dlsw.cn.enumerate.RoleType;
 import com.dlsw.cn.po.User;
 import com.dlsw.cn.web.service.OrderService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import com.dlsw.cn.web.service.imp.OrderServiceImp;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.io.File;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockRunnerDelegate(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ShoppingApplication.class)
-@PowerMockIgnore({"javax.management.*","org.apache.http.conn.ssl.*", "javax.net.ssl.*" , "javax.crypto.*"})
-public class DirectorLevelTest{
+@SpringBootTest(classes = {ShoppingApplication.class})
+public class DirectorLevelTest extends AbstractTestNGSpringContextTests{
     @Autowired
-    OrderService orderService;
+    private OrderService orderService;
+    @BeforeMethod
+    public void init(){
+        MockitoAnnotations.initMocks(OrderServiceImp.class);
+    }
+    @Test
+    public void testSureOrderCase() {
+        orderService.sureOrder("1123",444);
+    }
     @Test
     public void testGetDirectorLevel() {
         User user = mock(User.class);
         when(user.getLower()).thenReturn(addLowerUser(1));
-        org.junit.Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.无);
+        Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.无);
         when(user.getLower()).thenReturn(addLowerUser(2));
-        org.junit.Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.仁);
+        Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.仁);
         when(user.getLower()).thenReturn(addLowerUser(4));
-        org.junit.Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.义);
+        Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.义);
         when(user.getLower()).thenReturn(addLowerUser(6));
-        org.junit.Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.礼);
+        Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.礼);
         when(user.getLower()).thenReturn(addLowerUser(8));
-        org.junit.Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.智);
+        Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.智);
         when(user.getLower()).thenReturn(addLowerUser(10));
-        org.junit.Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.信);
+        Assert.assertEquals(DirectorLevel.getDirectorLevel(user), DirectorLevel.信);
     }
 
     private Set<User> addLowerUser(int count) {
@@ -58,17 +60,4 @@ public class DirectorLevelTest{
         }
         return userSet;
     }
-
-    @Test
-    @PrepareForTest(Source.class)
-    public void testCallInternalInstance() throws Exception {
-        File file = mock(File.class);
-        Source underTest = new Source();
-        whenNew(File.class).withArguments("bbb").thenReturn(file);
-        when(file.exists()).thenReturn(true);
-        boolean flag =underTest.callInternalInstance("bbb");
-        org.junit.Assert.assertEquals(flag,true);
-    }
-
-
 }
